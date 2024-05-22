dc1ba9413c0326a06b3ae9597a3ba6ec0f467210
linko-iheb <iheblinko51@gmail.com>
2024-05-22 15:14:25 +0100

    testing

--- index.js
+++ index.js
@@ -1,44 +1,29 @@
-// Example express application adding the parse-server module to expose Parse
-// compatible API routes.
-
 import express from 'express';
-import { ParseServer } from 'parse-server';
+import { ParseServer, RedisCacheAdapter } from 'parse-server';
 import path from 'path';
-const __dirname = path.resolve();
 import http from 'http';
-import { RedisCacheAdapter } from 'parse-server';
 
+const __dirname = path.resolve();
+
+// Initialize Redis Cache Adapter
 const redisUrl = process.env.REDIS_URL || 'redis://redis:6379';
-const redisOptions = { url: redisUrl }; // You can include other Redis options here
+const redisOptions = { url: redisUrl };
 const redisCacheAdapter = new RedisCacheAdapter(redisOptions);
 
-// Event listener function to handle Redis connection
-function onRedisConnect() {
-  console.log('Connected to Redis cache.');
-}
-
-// Event listener function to handle Redis connection errors
-function onRedisError(error) {
-  console.error('Error connecting to Redis:', error);
-}
-
 const config = {
-  databaseURI: process.env.DATABASE_URI || process.env.MONGODB_URI || 'mongodb://localhost:27017/dev',
+  databaseURI: process.env.DATABASE_URI || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || (__dirname + '/cloud/main.js'),
   appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', // Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse', // Don't forget to change to https if needed
+  masterKey: process.env.MASTER_KEY || '', // Keep it secret!
+  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse', // Change to https if needed
   cacheAdapter: redisCacheAdapter,
   liveQuery: {
     classNames: ['Posts', 'Comments'], // List of classes to support for query subscriptions
   },
 };
 
-export const app = express();
-
+const app = express();
 app.set('trust proxy', true);
-
-// Serve static assets from the /public folder
 app.use('/public', express.static(path.join(__dirname, '/public')));
 
 if (!process.env.TESTING) {
@@ -50,7 +35,6 @@ if (!process.env.TESTING) {
 
 app.get('/', async function (req, res) {
   try {
-    // Call the 'hello' Cloud Function
     const result = await Parse.Cloud.run('hello');
     res.status(200).send(result);
   } catch (error) {
@@ -66,12 +50,6 @@ app.get('/test', function (req, res) {
 if (!process.env.TESTING) {
   const port = process.env.PORT || 1337;
   const httpServer = http.createServer(app);
-
-  // Attach listener for Redis connection event
-  redisCacheAdapter.client.on('connect', onRedisConnect);
-
-  // Attach listener for Redis connection error event
-  redisCacheAdapter.client.on('error', onRedisError);
 
   httpServer.listen(port, function () {
     console.log('parse-server-example running on port ' + port + '.');
